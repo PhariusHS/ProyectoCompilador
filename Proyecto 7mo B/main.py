61ccd308cbc6d2820e07694c803b6972cb9329ca
PhariusHS <91559691+PhariusHS@users.noreply.github.com>
2023-06-29 11:07:55 -0300

    Agregacion estructuras repetitivas
    :D

--- Proyecto 7mo B/main.py
+++ Proyecto 7mo B/main.py
@@ -9,7 +9,7 @@ user_windows = str(popen("echo %USERNAME%").read()).replace("\n", "")
 #window
 window = Tk()
 window.title("ADC")
-window.geometry("1920x1080")
+window.geometry("720x480")
 
 
 #Checkeo si existe python dentro de la pc
@@ -28,16 +28,25 @@ def traducir(archivo="C:/Users/{}/Documents/adc.adc".format(str(user_windows))):
     content = archivo_abierto.read()
     archivo_abierto.close()
 
-    #remplazo keywords
+    #Remplazo keyword basicas
     content = content.replace("MOSTRAR", "print")
+    content = content.replace("ENTERO", "int")
+    content = content.replace("DECIMAL", "float")
+    content = content.replace("TEXTO", "string")
     content = content.replace("DICCIONARIO", "dic")
     content = content.replace("LISTA", "list")
     content = content.replace("CLASE", "class")
     content = content.replace("SENO", "sin")
     content = content.replace("COSENO", "cos")
+#Keywords avanzadas
+    content = content.replace("NADA","none")
     content = content.replace("INSERTAR", "insert")
     content = content.replace("AGREGAR", "extend")
     content = content.replace("ELIMINAR", "remove")
+    content = content.replace("BORRAR", "del")
+    content = content.replace("INTENTA","try")
+    content = content.replace("EXCEPTO","except")
+    content = content.replace("FINALMENTE","finally")
     content = content.replace("VERDADERO", "true")
     content = content.replace("FALSO", "false")
     content = content.replace("SI", "if")
@@ -49,7 +58,6 @@ def traducir(archivo="C:/Users/{}/Documents/adc.adc".format(str(user_windows))):
     content = content.replace("NI", "or")
     content = content.replace("ROMPER", "break")
     content = content.replace("DEVOLVER", "return")
-
     return content
 
 
@@ -59,13 +67,14 @@ def compilar():
     pass
 
 
+#Ejecucion y lectura de archivos ".adc" como ".py"
 def ejecutar():
     archivo = askopenfilename(title="Ejecutar", filetypes=(("academic script" , "*.adc") ,), initialdir="C:/Users/{}/Documents".format(str(user_windows)))
     traduction = traducir(archivo)
     py = open("ejecutador.py", "w")   
     py.write(traduction)
     py.close()
-    borrado = popen("cls").read().replace("\n","")
+        borrado = popen("cls").read().replace("\n","")
     if borrado.startswith("\"cls\""):
         system("clear")
     else:
@@ -76,27 +85,17 @@ def ejecutar():
 
 
 
-
 #title label
 tittle_label = ttk.Label(master = window, text ="Compilador", font=('Comic Sans MS', 36, 'bold italic'))
 tittle_label.pack()
 
-#Buttons
+#Buttons label
 buttons_frame = ttk.Frame(master=window)
+#Buttons
 button = ttk.Button(master=buttons_frame, text = "Ejecutar", command=lambda:ejecutar())
 buttons_frame.pack(side = "left")
 
 
 button.pack()
-
-
-
-
-
-
-
-
-
-
 #Window-run
 window.mainloop()
