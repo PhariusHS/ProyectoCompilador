f9a81671ea4490eb86cfcbef5407537e36f29535
PhariusHS <91559691+PhariusHS@users.noreply.github.com>
2023-06-29 10:44:18 -0300

    Nuevas funciones
    Agregado funciones intentar - excepto- finalmente - none

--- Proyecto 7mo B/main.py
+++ Proyecto 7mo B/main.py
@@ -9,7 +9,7 @@ user_windows = str(popen("echo %USERNAME%").read()).replace("\n", "")
 #window
 window = Tk()
 window.title("ADC")
-window.geometry("1920x1080")
+window.geometry("720x480")
 
 
 #Checkeo si existe python dentro de la pc
@@ -28,28 +28,25 @@ def traducir(archivo="C:/Users/{}/Documents/adc.adc".format(str(user_windows))):
     content = archivo_abierto.read()
     archivo_abierto.close()
 
-    #remplazo keywords
+    #Remplazo keyword basicas
     content = content.replace("MOSTRAR", "print")
+    content = content.replace("ENTERO", "int")
+    content = content.replace("DECIMAL", "float")
+    content = content.replace("TEXTO", "string")
     content = content.replace("DICCIONARIO", "dic")
     content = content.replace("LISTA", "list")
     content = content.replace("CLASE", "class")
     content = content.replace("SENO", "sin")
     content = content.replace("COSENO", "cos")
+#Keywords avanzadas
+    content = content.replace("NADA","none")
     content = content.replace("INSERTAR", "insert")
     content = content.replace("AGREGAR", "extend")
     content = content.replace("ELIMINAR", "remove")
-    content = content.replace("VERDADERO", "true")
-    content = content.replace("FALSO", "false")
-    content = content.replace("SI", "if")
-    content = content.replace("SINO", "else")
-    content = content.replace("PARA", "for")
-    content = content.replace("MIENTRAS", "while")
-    content = content.replace("SINO_ENTONCES", "elif")
-    content = content.replace("TAMBIEN", "and")
-    content = content.replace("NI", "or")
-    content = content.replace("ROMPER", "break")
-    content = content.replace("DEVOLVER", "return")
-
+    content = content.replace("BORRAR", "del")
+    content = content.replace("INTENTA","try")
+    content = content.replace("EXCEPTO","except")
+    content = content.replace("FINALMENTE","finally")
     return content
 
 
@@ -59,20 +56,15 @@ def compilar():
     pass
 
 
+#Ejecucion y lectura de archivos ".adc" como ".py"
 def ejecutar():
     archivo = askopenfilename(title="Ejecutar", filetypes=(("academic script" , "*.adc") ,), initialdir="C:/Users/{}/Documents".format(str(user_windows)))
     traduction = traducir(archivo)
     py = open("ejecutador.py", "w")   
     py.write(traduction)
     py.close()
-    borrado = popen("cls").read().replace("\n","")
-    if borrado.startswith("\"cls\""):
-        system("clear")
-    else:
-        system("cls")
     system("py ejecutador.py")
     remove("ejecutador.py")
-
 
 
 
@@ -81,22 +73,13 @@ def ejecutar():
 tittle_label = ttk.Label(master = window, text ="Compilador", font=('Comic Sans MS', 36, 'bold italic'))
 tittle_label.pack()
 
-#Buttons
+#Buttons label
 buttons_frame = ttk.Frame(master=window)
+#Buttons
 button = ttk.Button(master=buttons_frame, text = "Ejecutar", command=lambda:ejecutar())
 buttons_frame.pack(side = "left")
 
 
 button.pack()
-
-
-
-
-
-
-
-
-
-
 #Window-run
 window.mainloop()
